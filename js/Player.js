51c11a9fadd0c7a1dc9670195ffb011bb6e6ee37
Sakshishinde2009 <119950720+Sakshishinde2009@users.noreply.github.com>
2023-03-21 20:37:53 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,26 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+ getCount(){
+  var playerCountRef = database.ref("playerCont");
+    playerCountRef.on("value" , data => {
+    playerCount = data.val();
+  })
+ }
+  
+ updateCount(){
+  database.ref("/").update({
+    playerCount : count
+    });
+ }
+  
+}
+
+
+ 
+
